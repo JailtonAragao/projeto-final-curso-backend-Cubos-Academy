1828d5ff19d8cb34be47d25baddd06e0f566e84d
Diego Wesley Miranda de Andrade <diegowesley6@gmail.com>
2022-07-23 00:05:00 -0300

    Ajustes na rota de usuarios

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
-const { validarToken } = require('../middleware/validarToken');
+const validarToken = require('../middleware/validarToken');
+const { cadastrarUsuarios } = require('../controller/usuarios');
 
 const rotas = express();
 
-rotas.post('/',);
+rotas.post('/', cadastrarUsuarios);
 rotas.path('/redefinir',);
 
 rotas.use(validarToken);
