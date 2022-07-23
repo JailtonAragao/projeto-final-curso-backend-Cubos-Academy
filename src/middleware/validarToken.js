f54e1d11e06832e3122b122da7044374da800758
Diego Wesley Miranda de Andrade <diegowesley6@gmail.com>
2022-07-23 00:00:59 -0300

    Criação da validação de Token para acessos aos endpoints

--- src/middleware/validarToken.js
+++ src/middleware/validarToken.js
@@ -3,40 +3,38 @@ const knex = require('knex');
 
 const fraseSecreta = require('../chave_secreta_jwt');
 
-
 const validarToken = async (req, res, next) => {
     const { authorization: autorizacao } = req.headers;
 
     if (!autorizacao) {
-        return res.status(401).json({ messagem: 'O usuário deve está logado' });
+        return res.status(401).json({ messagem: 'O usuário deve efetuar o login' });
     }
 
     try {
-        let token = autorizacao.replace('Bearer ', "").trim();
+        const token = authorization.replace('Bearer ', '').trim();
 
-        let { dadosUsuario } = jwt.verify(token, fraseSecreta);
+        const { id } = jwt.verify(token, fraseSecreta);
 
-        console.log(`O usuário ${dadosUsuario.nome} está utilizando o sistema`);
+        const usuario = await knex('usuarios').where({ id }).first();
 
-        // concluir o token.
+        if (!usuario) {
+            return res.status(404).json('Usuario não encontrado');
+        }
+
+        const { senha, ...dadosUsuario } = usuario;
+
+        req.usuario = dadosUsuario;
 
         next();
-
     } catch (error) {
-
-        if (error.message === 'invalid signature') {
-            return res.status(400).json({ messagem: 'O usuário deve passar um token válido.' });
+        if (error.message === "jwt expired") {
+            return res.status(400).json('Token expirado, informar um novo')
         }
-        if (error.message === 'jwt malformed') {
-            return res.status(400).json({ messagem: 'O token deve ser informado.' });
+        if (error.message === "jwt malformed") {
+            return res.status(400).json('Informar um token válido')
         }
-        if (error.message === 'jwt expired') {
-            return res.status(400).json({ messagem: 'O token expirou, adicione um novo' });
-        }
-
-        return res.status(400).json({ messagem: error.message });
+        return res.status(400).json(error.message);
     }
-
 }
 
-module.exports = { validarToken }
+module.exports = validarToken;
