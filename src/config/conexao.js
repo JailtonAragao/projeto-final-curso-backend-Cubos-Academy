e7396210224e01974a6817606619274418a24f48
jailton aragao <jailton.aragao@gmail.com>
2022-07-24 16:19:12 -0300

    Splint teste de comexão categoria ok.

--- src/config/conexao.js
+++ src/config/conexao.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        user: process.env.DB_USER,
-        host: process.env.DB_HOST,
-        database: process.env.DB_DATABASE,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT,
+        user: 'postgres',
+        host: 'localhost',
+        database: 'pdv',
+        password: 'apollo21',
+        port: 5432,
     }
 });
 
--- src/controller/categorias.js
+++ src/controller/categorias.js
@@ -1,8 +1,8 @@
 const knex = require('../config/conexao');
 
 const listarCategoria = async (req, res) => {
-    const { id } = req.categorias;
-    return res.status(200).json(req.categorias);
+    const listarCategoria = await knex('categorias');
+    return res.status(200).json(listarCategoria);
 
 };
 
