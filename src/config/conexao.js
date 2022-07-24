98112fe7859d11fda56daec6158d5a635ca888b5
Diego Wesley Miranda de Andrade <diegowesley6@gmail.com>
2022-07-23 22:20:42 -0300

    Teste deploy

--- src/config/conexao.js
+++ src/config/conexao.js
@@ -3,11 +3,16 @@ require('dotenv').config();
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        user: 'sfnbyyociqqyns',
-        host: 'ec2-54-87-179-4.compute-1.amazonaws.com',
-        database: 'd93klr463f6iiq',
-        password: 'da520350f1793b55b18e9c9c4dced9820f977a637818540aa60f3ebcf4fe5aa7',
-        port: 5432,
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_DATABASE,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+        // user: 'sfnbyyociqqyns',
+        // host: 'ec2-54-87-179-4.compute-1.amazonaws.com',
+        // database: 'd93klr463f6iiq',
+        // password: 'da520350f1793b55b18e9c9c4dced9820f977a637818540aa60f3ebcf4fe5aa7',
+        // port: 5432,
         ssl: {
             rejectUnauthorized: false,
         }
