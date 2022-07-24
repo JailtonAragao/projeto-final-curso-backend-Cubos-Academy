19c9b358ce75b44a83d69318695f9dec9274478b
Diego Wesley Miranda de Andrade <diegowesley6@gmail.com>
2022-07-24 19:36:12 -0300

    Ajustes nos status code do catch

--- src/middleware/validarToken.js
+++ src/middleware/validarToken.js
@@ -28,12 +28,12 @@ const validarToken = async (req, res, next) => {
         next();
     } catch (error) {
         if (error.message === "jwt expired") {
-            return res.status(400).json('Token expirado, informar um novo')
+            return res.status(500).json('Token expirado, informar um novo')
         }
         if (error.message === "jwt malformed") {
-            return res.status(400).json('Informar um token válido')
+            return res.status(500).json('Informar um token válido')
         }
-        return res.status(400).json(error.message);
+        return res.status(500).json(error.message);
     }
 }
 
