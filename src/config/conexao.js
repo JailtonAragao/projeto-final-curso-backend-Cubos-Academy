b7740937650f2bb4e423cd3a0a4086adb5793b0e
Diego Wesley Miranda de Andrade <96137045+diegoandrade2022@users.noreply.github.com>
2022-07-24 22:29:05 -0300

    Ajuste na conexão
    Comentando parte do arquivo que não comentei

--- src/config/conexao.js
+++ src/config/conexao.js
@@ -7,7 +7,7 @@ const knex = require('knex')({
         // host: process.env.DB_HOST,
         // database: process.env.DB_DATABASE,
         // password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT,
+        // port: process.env.DB_PORT,
         user: 'sfnbyyociqqyns',
         host: 'ec2-54-87-179-4.compute-1.amazonaws.com',
         database: 'd93klr463f6iiq',
