d8c5d7b1b9dd3adf8cbd2c84695788931d3d65c5
Diego Wesley Miranda de Andrade <diegowesley6@gmail.com>
2022-07-26 12:58:55 -0300

    Ajuste final config mailgun

--- src/nodemailer.js
+++ src/nodemailer.js
@@ -4,17 +4,10 @@ require('dotenv').config();
 const transportador = nodemailer.createTransport({
     host: process.env.MG_HOST,
     port: process.env.MG_PORT,
-    //secure: process.env.MG_SECURE, // true for 465, false for other ports
     auth: {
         user: process.env.MG_USER, // generated ethereal user
         pass: process.env.MG_PASS, // generated ethereal password
     },
-    //host:  ,
-    //port: ,
-    //auth: {
-    // user:  ,
-    // pass: ,
-    // },
 });
 
 module.exports = transportador;  
