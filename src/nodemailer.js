ca22950eff8e3a2d7e1c526b4ed40900b93220d9
Diego Wesley Miranda de Andrade <96137045+diegoandrade2022@users.noreply.github.com>
2022-07-26 11:58:02 -0300

    Ajuste no process.env, erro na atribuição

--- src/nodemailer.js
+++ src/nodemailer.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 const transportador = nodemailer.createTransport({
     host: process.env.MG_HOST,
-    port: process.env.DB_PORT,
-    secure: process.env.DB_SECURE, // true for 465, false for other ports
+    port: process.env.MG_PORT,
+    secure: process.env.MG_SECURE, // true for 465, false for other ports
     auth: {
-        user: process.env.DB_USER, // generated ethereal user
-        pass: process.env.DB_PASS, // generated ethereal password
+        user: process.env.MG_USER, // generated ethereal user
+        pass: process.env.MG_PASS, // generated ethereal password
     },
 });
 
