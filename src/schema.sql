57adeb7245d3a2cd0b4b0df70765b9fad2d53c63
Diego Wesley Miranda de Andrade <diegowesley6@gmail.com>
2022-07-28 20:43:31 -0300

    Atualização do banco para segunda strint

--- src/schema.sql
+++ src/schema.sql
@@ -28,3 +28,26 @@ insert into categorias (descricao)
 values ('Bebê');
 insert into categorias (descricao)
 values ('Games');
+
+
+create table produtos (
+	id serial primary key,
+	descricao text not null,
+	quantidade_estoque int not null,
+	valor int not null,
+	categoria_id int not null,
+	foreign key (categoria_id) references categorias(id)
+);
+
+create table clientes (
+	id serial primary key,
+	nome varchar(100) not null,
+	email varchar(100) not null unique,
+	cpf varchar(11) not null unique,
+	cep int not null,
+	rua text not null,
+	numero int not null,
+	bairro text not null,
+	cidade text not null,
+	estado varchar(2) not null
+);
