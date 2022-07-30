1891ec1d3486959551df1304207a1830c1968eab
Renan <rcaliga@gmail.com>
2022-07-30 11:11:30 -0300

    removendo comentários

--- src/controller/clientes.js
+++ src/controller/clientes.js
@@ -38,22 +38,6 @@ const editarCliente = async (req, res) => {
         } else {
             return res.status(404).json({ 'mensagem': 'O cpf informado já consta em nosso banco de dados' });
         }
-        
-        
-        
-        // if (!emailExiste || emailExiste.id == 1) {
-            
-        //     const clienteAtualizado = await knex('clientes').update({ nome, email, cpf }).where({ id });
-
-        //     if (!clienteAtualizado) {
-        //         return res.status(404).json({ 'mensagem': 'O cliente não foi atualizado' });
-        //     }
-
-        //     return res.status(200).json({ 'mensagem': "Cliente atualizado com Sucesso!" });
-            
-        // } else {
-        //     return res.status(404).json({ 'mensagem': 'O email informado já consta em nosso banco de dados' });
-        // }
 
     } catch (error) {
         return res.status(500).json({ 'mensagem': error.message });
