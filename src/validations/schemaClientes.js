d71b08578070f5c4d1cde07f51a6321736b4156d
Diego Wesley Miranda de Andrade <diegowesley6@gmail.com>
2022-07-29 21:51:17 -0300

    Ajustes

--- src/validations/schemaClientes.js
+++ src/validations/schemaClientes.js
@@ -3,10 +3,10 @@ const yup = require('./configuracoes');
 const schemaClientes = yup.object().shape({
     nome: yup.string().required('O campo nome é obrigatório'),
     email: yup.string().email().required('O campo email é obrigatório'),
-    cpf: yup.number().strict().required('O campo cpf é obrigatório').max(11),
-    cep: yup.number(),
+    cpf: yup.string().strict().required('O campo cpf é obrigatório').max(11),
+    cep: yup.string(),
     rua: yup.string(),
-    numero: yup.number(),
+    numero: yup.string(),
     bairro: yup.string(),
     cidade: yup.string(),
     estado: yup.string().max(2)
