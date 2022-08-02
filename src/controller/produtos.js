09abbb9ae7dda81e36dc8fb46b5e8694786d183a
Diego Wesley Miranda de Andrade <diegowesley6@gmail.com>
2022-08-01 22:26:18 -0300

    Teste finalizado, desistir por enquanto

--- src/controller/produtos.js
+++ src/controller/produtos.js
@@ -79,14 +79,9 @@ const listarProduto = async (req, res) => {
 
     try {
 
-
-
         if (!categoria_id) {
             const listarTodos = await knex('produtos');
             return res.status(200).json(listarTodos);
-        } else if (Array.isArray(categoria_id)) {
-            const arrayProdutoExiste = await knex('produtos').where('categoria_id', 'any', categoria_id);
-            return res.status(200).json(arrayProdutoExiste);
         } else {
             const produtoExiste = await knex('produtos').where({ categoria_id }).first();
 
