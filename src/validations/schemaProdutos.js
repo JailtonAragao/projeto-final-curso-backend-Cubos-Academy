9b495e3fc454b8ca249d3629bb9a93cf0a332a04
Diego Wesley Miranda de Andrade <diegowesley6@gmail.com>
2022-08-04 18:55:22 -0300

    Validação de valores negativos

--- src/validations/schemaProdutos.js
+++ src/validations/schemaProdutos.js
@@ -2,8 +2,8 @@ const yup = require('./configuracoes');
 
 const schemaCadastrarProdutos = yup.object().shape({
     descricao: yup.string().required('O descrição é obrigatório'),
-    quantidade_estoque: yup.number().required('O campo quantidade_estoque é obrigatório').min(1),
-    valor: yup.number().required('O campo valor é obrigatório').min(0),
+    quantidade_estoque: yup.number().required('O campo quantidade_estoque é obrigatório e tem que ter pelo menos 1 item').min(1),
+    valor: yup.number().required('O campo valor é obrigatório e não pode ser negativo').min(0),
     categoria_id: yup.number().required('O campo categoria_id é obrigatório')
 });
 
