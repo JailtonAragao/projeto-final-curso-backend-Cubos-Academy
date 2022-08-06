cce1474b3c0c2cf1aab7788b3e9f0103a67a03fb
Diego Wesley Miranda de Andrade <diegowesley6@gmail.com>
2022-08-05 23:05:08 -0300

    teste

--- src/validations/schemaUpload.js
+++ src/validations/schemaUpload.js
@@ -2,7 +2,9 @@ const yup = require('./configuracoes');
 
 const schemaUpload = yup.object().shape({
     url_produto: yup.string().required('O campo url_produto é obrigatório'),
-    imagem: yup.string().requered('O campo imagem é obrigatório')
+    imagem: yup.string().requered('O campo imagem é obrigatório'),
+    id_produto: yup.number().required('O campo id_produto é obrigatório')
+
 });
 
 module.exports = {
