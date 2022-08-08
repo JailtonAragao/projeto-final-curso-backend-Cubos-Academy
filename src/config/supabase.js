4b0df27bb8f8db568ad936437b40f738267c1ff2
Diego Wesley Miranda de Andrade <diegowesley6@gmail.com>
2022-08-08 20:18:10 -0300

    Teste supabase

--- src/config/supabase.js
+++
@@ -1,7 +0,0 @@
-const { createClient } = require('@supabase/supabase-js');
-
-const supabase = createClient(peocess.env.SUPABASE_URL, process.env.SUPABASE_KEY);
-
-module.exports = {
-    supabase
-}
