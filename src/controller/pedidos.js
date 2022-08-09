ca05d4e8faed9d6fa526a77779991b36c075b4be
Diego Wesley Miranda de Andrade <diegowesley6@gmail.com>
2022-08-08 23:42:34 -0300

    Ajuste

--- src/controller/pedidos.js
+++ src/controller/pedidos.js
@@ -66,7 +66,7 @@ const cadastrarPedido = async (req, res) => {
 
         } else {
             return res.status(404).json({
-                "mensagem": "Os Produtos a seguir não foram encontrados ou a quantidade de estoque é inválida",
+                mensagem: "Os Produtos a seguir não foram encontrados ou a quantidade de estoque é inválida",
                 "Produtos não encontrados": produtosNaoEncontrados,
                 "Produtos em estoque": produtosSemEstoque
             });
