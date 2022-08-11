8baf8bf883f015ab10ebbb8b77b7c4c868176bb3
Diego Wesley Miranda de Andrade <diegowesley6@gmail.com>
2022-08-10 22:02:01 -0300

    Ajustando config com process.env

--- src/config/conexao.js
+++ src/config/conexao.js
@@ -1,14 +1,16 @@
-
 require('dotenv').config();
 
 const knex = require('knex')({
     client: 'pg',
     connection: {
-        user: 'postgres',
-        host: 'localhost',
-        database: 'pdv',
-        password: '12345',
-        port: '5432'
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_DATABASE,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+        ssl: {
+            rejectUnauthorized: false,
+        }
     }
 });
 
